1e9bf352d1f01332f6088e656638ace25d3a5166
pengjie <2267258221@qq.com>
2021-07-25 16:01:18 +0800

    新增

---
+++ BUG/Python中常见的异常类型.py
@@ -0,0 +1,20 @@
+'''（1）数学运算异常,除(或取模)零(所有数据类型)'''
+# print(10/0) #ZeroDivisionError
+
+'''(2)序列中没有此索引(index)'''
+lst=[11,22,33,44]
+# print(lst[4]) #IndexError 索引从0开始
+
+'''(3)映射中没有这个键'''
+dic={'name':'张三','age':20}
+# print(dic['gender']) #KeyError
+
+'''(4)映射中没有这个键'''
+# print(num) #NameError
+
+'''(5)Python语法错误'''
+# int a=20 #SyntaxError
+
+'''(6)传入无效的参数'''
+# a=int('hello') #ValueError
+
---
+++ BUG/try-except-else-finally.py
@@ -0,0 +1,11 @@
+try:
+    a=int(input('请输入第一个整数'))
+    b=int(input('请输入第一二个整数'))
+    result=a/b
+    print('结果为:',result)
+except BaseException as e:
+    print('出错了',e)
+else:
+    print('计算结果为',result)
+finally:
+    print('谢谢您的使用')
