9d90ede25bcdfc8ba1a3528d8cc3ce91d1d2bda6
pengjie <2267258221@qq.com>
2021-07-27 22:46:56 +0800

    新增

---
+++ 类/导入calc.py
@@ -0,0 +1,11 @@
+# 在demo中导入calc自定义模块使用
+'''
+(1)
+import calc
+print(calc.add(10,20))
+print(calc.div(10,20))
+'''
+
+'''(2)'''
+from calc import add
+print(add(20,80))
---
+++ 类/模块的导入1.py
@@ -0,0 +1,11 @@
+from math import pi
+from math import pow
+# import math
+print(pi)
+print(pow(2,3))
+# print(math.pow(2,3)) 8.0
+'''
+导入模块
+import 模块名称 [as别名]
+from 模块名称 import 函数/变量/类
+'''
